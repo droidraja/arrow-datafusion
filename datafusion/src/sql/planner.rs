05cc54eff0d2dc668c83169739469cb2d698ee1b
Andrew Lamb <andrew@nerdnetworks.org>
2021-12-13 21:54:41 +0300

    Consolidate decimal error checking (#1438)
    Can drop this after rebase on commit 4b454d0, first released in 7.0.0

--- datafusion/src/sql/planner.rs
+++ datafusion/src/sql/planner.rs
@@ -46,7 +46,7 @@ use crate::logical_plan::{
 };
 use crate::prelude::JoinType;
 use crate::scalar::ScalarValue;
-use crate::sql::utils::find_rolling_aggregate_exprs;
+use crate::sql::utils::{find_rolling_aggregate_exprs, make_decimal_type};
 use crate::{
     error::{DataFusionError, Result},
     physical_plan::udaf::AggregateUDF,
@@ -295,25 +295,7 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
                 Ok(DataType::Utf8)
             }
             SQLDataType::Decimal(precision, scale) => {
-                match (precision, scale) {
-                    (None, _) | (_, None) => {
-                        return Err(DataFusionError::Internal(format!(
-                            "Invalid Decimal type ({:?}), precision or scale can't be empty.",
-                            sql_type
-                        )));
-                    }
-                    (Some(p), Some(s)) => {
-                        // TODO add bound checker in some utils file or function
-                        if *p > 38 || *s > *p {
-                            return Err(DataFusionError::Internal(format!(
-                                "Error Decimal Type ({:?}), precision must be less than or equal to 38 and scale can't be greater than precision",
-                                sql_type
-                            )));
-                        } else {
-                            Ok(DataType::Decimal(*p as usize, *s as usize))
-                        }
-                    }
-                }
+                make_decimal_type(*precision, *scale)
             }
             SQLDataType::Float(_) => Ok(DataType::Float32),
             SQLDataType::Real | SQLDataType::Double => Ok(DataType::Float64),
@@ -1957,27 +1939,7 @@ pub fn convert_data_type(sql_type: &SQLDataType) -> Result<DataType> {
         SQLDataType::Char(_) | SQLDataType::Varchar(_) => Ok(DataType::Utf8),
         SQLDataType::Timestamp => Ok(DataType::Timestamp(TimeUnit::Nanosecond, None)),
         SQLDataType::Date => Ok(DataType::Date32),
-        SQLDataType::Decimal(precision, scale) => {
-            match (precision, scale) {
-                (None, _) | (_, None) => {
-                    return Err(DataFusionError::Internal(format!(
-                        "Invalid Decimal type ({:?}), precision or scale can't be empty.",
-                        sql_type
-                    )));
-                }
-                (Some(p), Some(s)) => {
-                    // TODO add bound checker in some utils file or function
-                    if *p > 38 || *s > *p {
-                        return Err(DataFusionError::Internal(format!(
-                            "Error Decimal Type ({:?})",
-                            sql_type
-                        )));
-                    } else {
-                        Ok(DataType::Decimal(*p as usize, *s as usize))
-                    }
-                }
-            }
-        }
+        SQLDataType::Decimal(precision, scale) => make_decimal_type(*precision, *scale),
         other => Err(DataFusionError::NotImplemented(format!(
             "Unsupported SQL type {:?}",
             other
