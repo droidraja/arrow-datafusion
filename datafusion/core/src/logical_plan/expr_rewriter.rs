fb553e2a3d0ff96158420280bd9ab38462c18fb1
George <g.andronchik@gmail.com>
2022-08-02 20:27:47 +0800

    refactor

--- datafusion/core/src/logical_plan/expr_rewriter.rs
+++ datafusion/core/src/logical_plan/expr_rewriter.rs
@@ -329,14 +329,18 @@ fn rewrite_sort_col_by_aggs(expr: Expr, plan: &LogicalPlan) -> Result<Expr> {
             expr: projection_expr,
             ..
         }) => {
-            let res = rebase_expr(&expr, projection_expr.as_slice(), input)?;
+            let res = normalize_col(
+                unnormalize_col(rebase_expr(&expr, projection_expr.as_slice(), input)?),
+                plan,
+            )?;
             let alias_map = extract_aliased_expr_names(&projection_expr, input.schema());
-            let rewritten_res =
-                resolve_exprs_to_aliases(&res, &alias_map, input.schema())?;
-            let res = rebase_expr(&rewritten_res, projection_expr.as_slice(), plan)
-                .unwrap_or(res);
-            let res = rewrite_sort_col_by_aggs(res, input)?;
-            Ok(res)
+            let res = resolve_exprs_to_aliases(&res, &alias_map, input.schema())?;
+
+            Ok(if let LogicalPlan::Aggregate(_) = **input {
+                rewrite_sort_col_by_aggs(res, input)?
+            } else {
+                res
+            })
         }
         _ => Ok(expr),
     }
