apache/datafusion

Support limit push down for offset_plan (#2566)
* support offset push down

* change the limit and offset order

* add test for join and subquery

* fmt

* Apply suggestions from code review

Co-authored-by: Andy Grove <andygrove73@gmail.com>

* add sql test in planner.rs

* add sql test in planner.rs

* fix clippy

Co-authored-by: Andy Grove <andygrove73@gmail.com>

datafusion/core/src/optimizer/limit_push_down.rs
@@ -24,6 +24,7 @@ use crate::logical_plan::plan::Projection;
 use crate::logical_plan::{Limit, TableScan};
 use crate::logical_plan::{LogicalPlan, Union};
 use crate::optimizer::optimizer::OptimizerRule;
+use datafusion_expr::logical_plan::Offset;
 use std::sync::Arc;
 
 /// Optimization rule that tries pushes down LIMIT n
@@ -43,18 +44,24 @@ fn limit_push_down(
     upper_limit: Option<usize>,
     plan: &LogicalPlan,
     _execution_props: &ExecutionProps,
+    is_offset: bool,
 ) -> Result<LogicalPlan> {
     match (plan, upper_limit) {
         (LogicalPlan::Limit(Limit { n, input }), upper_limit) => {
-            let smallest = upper_limit.map(|x| std::cmp::min(x, *n)).unwrap_or(*n);
+            let new_limit: usize = if is_offset {
+                *n + upper_limit.unwrap_or(0)
+            } else {
+                upper_limit.map(|x| std::cmp::min(x, *n)).unwrap_or(*n)
+            };
             Ok(LogicalPlan::Limit(Limit {
-                n: smallest,
+                n: new_limit,
                 // push down limit to plan (minimum of upper limit and current limit)
                 input: Arc::new(limit_push_down(
                     _optimizer,
-                    Some(smallest),
+                    Some(new_limit),
                     input.as_ref(),
                     _execution_props,
+                    false,
                 )?),
             }))
         }
@@ -95,6 +102,7 @@ fn limit_push_down(
                     upper_limit,
                     input.as_ref(),
                     _execution_props,
+                    false,
                 )?),
                 schema: schema.clone(),
                 alias: alias.clone(),
@@ -119,6 +127,7 @@ fn limit_push_down(
                             Some(upper_limit),
                             x,
                             _execution_props,
+                            false,
                         )?),
                     }))
                 })
@@ -127,6 +136,25 @@ fn limit_push_down(
                 inputs: new_inputs,
                 alias: alias.clone(),
                 schema: schema.clone(),
+            }))
+        }
+        // offset 5 limit 10 then push limit 15 (5 + 10)
+        // Limit should always be Offset's input
+        (LogicalPlan::Offset(Offset { offset, input }), upper_limit) => {
+            let new_limit = if let Some(ul) = upper_limit {
+                ul + *offset
+            } else {
+                *offset
+            };
+            Ok(LogicalPlan::Offset(Offset {
+                offset: *offset,
+                input: Arc::new(limit_push_down(
+                    _optimizer,
+                    Some(new_limit),
+                    input.as_ref(),
+                    _execution_props,
+                    true,
+                )?),
             }))
         }
         // For other nodes we can't push down the limit
@@ -138,7 +166,9 @@ fn limit_push_down(
             let inputs = plan.inputs();
             let new_inputs = inputs
                 .iter()
-                .map(|plan| limit_push_down(_optimizer, None, plan, _execution_props))
+                .map(|plan| {
+                    limit_push_down(_optimizer, None, plan, _execution_props, false)
+                })
                 .collect::<Result<Vec<_>>>()?;
 
             utils::from_plan(plan, &expr, &new_inputs)
@@ -152,7 +182,7 @@ impl OptimizerRule for LimitPushDown {
         plan: &LogicalPlan,
         execution_props: &ExecutionProps,
     ) -> Result<LogicalPlan> {
-        limit_push_down(self, None, plan, execution_props)
+        limit_push_down(self, None, plan, execution_props, false)
     }
 
     fn name(&self) -> &str {
@@ -167,6 +197,8 @@ mod test {
         logical_plan::{col, max, LogicalPlan, LogicalPlanBuilder},
         test::*,
     };
+    use datafusion_expr::exists;
+    use datafusion_expr::logical_plan::JoinType;
 
     fn assert_optimized_plan_eq(plan: &LogicalPlan, expected: &str) {
         let rule = LimitPushDown::new();
@@ -275,6 +307,174 @@ mod test {
         \n      TableScan: test projection=None, limit=1000";
 
         assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_pushdown_with_offset_projection_table_provider() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .project(vec![col("a")])?
+            .offset(10)?
+            .limit(1000)?
+            .build()?;
+
+        // Should push the limit down to table provider
+        // When it has a select
+        let expected = "Limit: 1000\
+        \n  Offset: 10\
+        \n    Projection: #test.a\
+        \n      TableScan: test projection=None, limit=1010";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_pushdown_with_offset_after_limit() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .project(vec![col("a")])?
+            .limit(1000)?
+            .offset(10)?
+            .build()?;
+
+        let expected = "Offset: 10\
+        \n  Limit: 1010\
+        \n    Projection: #test.a\
+        \n      TableScan: test projection=None, limit=1010";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_push_down_with_offset_take_smaller_limit() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .offset(10)?
+            .limit(1000)?
+            .limit(10)?
+            .build()?;
+
+        // Should push down the smallest limit
+        // Towards table scan
+        // This rule doesn't replace multiple limits
+        let expected = "Limit: 10\
+        \n  Limit: 10\
+        \n    Offset: 10\
+        \n      TableScan: test projection=None, limit=20";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_doesnt_push_down_with_offset_aggregation() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .aggregate(vec![col("a")], vec![max(col("b"))])?
+            .offset(10)?
+            .limit(1000)?
+            .build()?;
+
+        // Limit should *not* push down aggregate node
+        let expected = "Limit: 1000\
+        \n  Offset: 10\
+        \n    Aggregate: groupBy=[[#test.a]], aggr=[[MAX(#test.b)]]\
+        \n      TableScan: test projection=None";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_should_push_down_with_offset_union() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan.clone())
+            .union(LogicalPlanBuilder::from(table_scan).build()?)?
+            .offset(10)?
+            .limit(1000)?
+            .build()?;
+
+        // Limit should push down through union
+        let expected = "Limit: 1000\
+        \n  Offset: 10\
+        \n    Union\
+        \n      Limit: 1010\
+        \n        TableScan: test projection=None, limit=1010\
+        \n      Limit: 1010\
+        \n        TableScan: test projection=None, limit=1010";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_should_not_push_down_with_offset_join() -> Result<()> {
+        let table_scan_1 = test_table_scan()?;
+        let table_scan_2 = test_table_scan_with_name("test2")?;
+
+        let plan = LogicalPlanBuilder::from(table_scan_1)
+            .join(
+                &LogicalPlanBuilder::from(table_scan_2).build()?,
+                JoinType::Left,
+                (vec!["a"], vec!["a"]),
+            )?
+            .limit(1000)?
+            .offset(10)?
+            .build()?;
+
+        // Limit pushdown Not supported in Join
+        let expected = "Offset: 10\
+        \n  Limit: 1010\
+        \n    Left Join: #test.a = #test2.a\
+        \n      TableScan: test projection=None\
+        \n      TableScan: test2 projection=None";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_should_not_push_down_with_offset_sub_query() -> Result<()> {
+        let table_scan_1 = test_table_scan_with_name("test1")?;
+        let table_scan_2 = test_table_scan_with_name("test2")?;
+
+        let subquery = LogicalPlanBuilder::from(table_scan_1)
+            .project(vec![col("a")])?
+            .filter(col("a").eq(col("test1.a")))?
+            .build()?;
+
+        let outer_query = LogicalPlanBuilder::from(table_scan_2)
+            .project(vec![col("a")])?
+            .filter(exists(Arc::new(subquery)))?
+            .limit(100)?
+            .offset(10)?
+            .build()?;
+
+        // Limit pushdown Not supported in sub_query
+        let expected = "Offset: 10\
+        \n  Limit: 110\
+        \n    Filter: EXISTS (Subquery: Filter: #test1.a = #test1.a\
+        \n  Projection: #test1.a\
+        \n    TableScan: test1 projection=None)\
+        \n      Projection: #test2.a\
+        \n        TableScan: test2 projection=None";
+
+        assert_optimized_plan_eq(&outer_query, expected);
 
         Ok(())
     }

droidraja/arrow-datafusion

datafusion/core/src/optimizer/limit_push_down.rs
@@ -21,6 +21,7 @@ use super::utils;
 use crate::error::Result;
 use crate::execution::context::ExecutionProps;
 use crate::logical_plan::plan::Projection;
+use crate::logical_plan::Offset;
 use crate::logical_plan::{Limit, TableScan};
 use crate::logical_plan::{LogicalPlan, Union};
 use crate::optimizer::optimizer::OptimizerRule;
@@ -39,22 +40,28 @@ impl LimitPushDown {
 }
 
 fn limit_push_down(
-    optimizer: &LimitPushDown,
+    _optimizer: &LimitPushDown,
     upper_limit: Option<usize>,
     plan: &LogicalPlan,
-    execution_props: &ExecutionProps,
+    _execution_props: &ExecutionProps,
+    is_offset: bool,
 ) -> Result<LogicalPlan> {
     match (plan, upper_limit) {
         (LogicalPlan::Limit(Limit { n, input }), upper_limit) => {
-            let smallest = upper_limit.map(|x| std::cmp::min(x, *n)).unwrap_or(*n);
+            let new_limit: usize = if is_offset {
+                *n + upper_limit.unwrap_or(0)
+            } else {
+                upper_limit.map(|x| std::cmp::min(x, *n)).unwrap_or(*n)
+            };
             Ok(LogicalPlan::Limit(Limit {
-                n: smallest,
+                n: new_limit,
                 // push down limit to plan (minimum of upper limit and current limit)
                 input: Arc::new(limit_push_down(
-                    optimizer,
-                    Some(smallest),
+                    _optimizer,
+                    Some(new_limit),
                     input.as_ref(),
-                    execution_props,
+                    _execution_props,
+                    false,
                 )?),
             }))
         }
@@ -91,10 +98,11 @@ fn limit_push_down(
             Ok(LogicalPlan::Projection(Projection {
                 expr: expr.clone(),
                 input: Arc::new(limit_push_down(
-                    optimizer,
+                    _optimizer,
                     upper_limit,
                     input.as_ref(),
-                    execution_props,
+                    _execution_props,
+                    false,
                 )?),
                 schema: schema.clone(),
                 alias: alias.clone(),
@@ -115,10 +123,11 @@ fn limit_push_down(
                     Ok(LogicalPlan::Limit(Limit {
                         n: upper_limit,
                         input: Arc::new(limit_push_down(
-                            optimizer,
+                            _optimizer,
                             Some(upper_limit),
                             x,
-                            execution_props,
+                            _execution_props,
+                            false,
                         )?),
                     }))
                 })
@@ -127,6 +136,25 @@ fn limit_push_down(
                 inputs: new_inputs,
                 alias: alias.clone(),
                 schema: schema.clone(),
+            }))
+        }
+        // offset 5 limit 10 then push limit 15 (5 + 10)
+        // Limit should always be Offset's input
+        (LogicalPlan::Offset(Offset { offset, input }), upper_limit) => {
+            let new_limit = if let Some(ul) = upper_limit {
+                ul + *offset
+            } else {
+                *offset
+            };
+            Ok(LogicalPlan::Offset(Offset {
+                offset: *offset,
+                input: Arc::new(limit_push_down(
+                    _optimizer,
+                    Some(new_limit),
+                    input.as_ref(),
+                    _execution_props,
+                    true,
+                )?),
             }))
         }
         // For other nodes we can't push down the limit
@@ -138,7 +166,9 @@ fn limit_push_down(
             let inputs = plan.inputs();
             let new_inputs = inputs
                 .iter()
-                .map(|plan| limit_push_down(optimizer, None, plan, execution_props))
+                .map(|plan| {
+                    limit_push_down(_optimizer, None, plan, _execution_props, false)
+                })
                 .collect::<Result<Vec<_>>>()?;
 
             utils::from_plan(plan, &expr, &new_inputs)
@@ -152,7 +182,7 @@ impl OptimizerRule for LimitPushDown {
         plan: &LogicalPlan,
         execution_props: &ExecutionProps,
     ) -> Result<LogicalPlan> {
-        limit_push_down(self, None, plan, execution_props)
+        limit_push_down(self, None, plan, execution_props, false)
     }
 
     fn name(&self) -> &str {
@@ -164,7 +194,7 @@ impl OptimizerRule for LimitPushDown {
 mod test {
     use super::*;
     use crate::{
-        logical_plan::{col, max, LogicalPlan, LogicalPlanBuilder},
+        logical_plan::{col, max, JoinType, LogicalPlan, LogicalPlanBuilder},
         test::*,
     };
 
@@ -273,6 +303,143 @@ mod test {
         \n  Aggregate: groupBy=[[#test.a]], aggr=[[MAX(#test.b)]]\
         \n    Limit: 1000\
         \n      TableScan: test projection=None, limit=1000";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_pushdown_with_offset_projection_table_provider() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .project(vec![col("a")])?
+            .offset(10)?
+            .limit(1000)?
+            .build()?;
+
+        // Should push the limit down to table provider
+        // When it has a select
+        let expected = "Limit: 1000\
+        \n  Offset: 10\
+        \n    Projection: #test.a\
+        \n      TableScan: test projection=None, limit=1010";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_pushdown_with_offset_after_limit() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .project(vec![col("a")])?
+            .limit(1000)?
+            .offset(10)?
+            .build()?;
+
+        let expected = "Offset: 10\
+        \n  Limit: 1010\
+        \n    Projection: #test.a\
+        \n      TableScan: test projection=None, limit=1010";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_push_down_with_offset_take_smaller_limit() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .offset(10)?
+            .limit(1000)?
+            .limit(10)?
+            .build()?;
+
+        // Should push down the smallest limit
+        // Towards table scan
+        // This rule doesn't replace multiple limits
+        let expected = "Limit: 10\
+        \n  Limit: 10\
+        \n    Offset: 10\
+        \n      TableScan: test projection=None, limit=20";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_doesnt_push_down_with_offset_aggregation() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan)
+            .aggregate(vec![col("a")], vec![max(col("b"))])?
+            .offset(10)?
+            .limit(1000)?
+            .build()?;
+
+        // Limit should *not* push down aggregate node
+        let expected = "Limit: 1000\
+        \n  Offset: 10\
+        \n    Aggregate: groupBy=[[#test.a]], aggr=[[MAX(#test.b)]]\
+        \n      TableScan: test projection=None";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_should_push_down_with_offset_union() -> Result<()> {
+        let table_scan = test_table_scan()?;
+
+        let plan = LogicalPlanBuilder::from(table_scan.clone())
+            .union(LogicalPlanBuilder::from(table_scan).build()?)?
+            .offset(10)?
+            .limit(1000)?
+            .build()?;
+
+        // Limit should push down through union
+        let expected = "Limit: 1000\
+        \n  Offset: 10\
+        \n    Union\
+        \n      Limit: 1010\
+        \n        TableScan: test projection=None, limit=1010\
+        \n      Limit: 1010\
+        \n        TableScan: test projection=None, limit=1010";
+
+        assert_optimized_plan_eq(&plan, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn limit_should_not_push_down_with_offset_join() -> Result<()> {
+        let table_scan_1 = test_table_scan()?;
+        let table_scan_2 = test_table_scan_with_name("test2")?;
+
+        let plan = LogicalPlanBuilder::from(table_scan_1)
+            .join(
+                &LogicalPlanBuilder::from(table_scan_2).build()?,
+                JoinType::Left,
+                (vec!["a"], vec!["a"]),
+            )?
+            .limit(1000)?
+            .offset(10)?
+            .build()?;
+
+        // Limit pushdown Not supported in Join
+        let expected = "Offset: 10\
+        \n  Limit: 1010\
+        \n    Left Join: #test.a = #test2.a\
+        \n      TableScan: test projection=None\
+        \n      TableScan: test2 projection=None";
 
         assert_optimized_plan_eq(&plan, expected);
 